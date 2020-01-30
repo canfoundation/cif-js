26e5d77ad4e1874dedef007ea35ef7b67e3e4002
Linh Tran <linhtran@lecle.co.kr>
2020-01-30 12:06:48 +0700

    update example for lib

--- examples/index.js
+++ examples/index.js
@@ -2,14 +2,18 @@ const fetch = require('node-fetch');
 const { CanCommunity } = require('cif-js');
 const { TextEncoder, TextDecoder } = require('util');
 
-const canCommunity = new CanCommunity('leonardo', '1.0', {
-  // @ts-ignore
-  fetch: fetch,
+const canCommunity = new CanCommunity({
   canUrl: 'http://18.182.95.163:8888',
-  // @ts-ignore
+  signOption: {
+    userId: '1',
+    signTrxMethod: 'CAN_PASS',
+    canAccount: 'leonardo',
+    communityCanAccount: 'Coin',
+  },
   textEncoder: new TextEncoder(),
   textDecoder: new TextDecoder(),
-  governanceAccount: 'governance23',
+  code: 'governance23',
+  fetch,
 });
 
 console.log('canCommunity', canCommunity);
